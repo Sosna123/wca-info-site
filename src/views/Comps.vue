0b9309aaee87e01ed573ee44f060d758f3932e43
Sosna123 <olek.arez@gmail.com>
2023-09-27 20:43:49 +0200

    a lot of changes to /comps

--- src/views/Comps.vue
+++ src/views/Comps.vue
@@ -1,51 +1,83 @@
 <template>
-    <input type="text" v-bind="countryTag">
+    <select name="countrySelect" v-model="countryTag">
+        <option disabled value="">Please select a country</option>
+        <option v-for="country in countries" :value="country.iso2Code">{{ country.name }}</option>
+    </select>
     <button @click="displayData">button</button>
     <ul>
-        <li v-for="comp in data.items">
-            <p v-if="!comp.isCanceled">{{ comp.name }}</p>
-            <!--  && comp.date.from > currentDate -->
-        </li>
+        <div>
+            <li v-for="comp in fData">
+                <a :href="`https://www.worldcubeassociation.org/competitions/${comp.id}`" target="_blank">
+                    <p class="compName compTxt">{{ comp.name }}</p>
+               </a>
+                <p class="compTxt">will be in {{ comp.city }} at {{ comp.venue.address }}</p>
+            </li>
+        </div>
     </ul>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { countries } from '../countries.js'
 
 export default{
     name: "comps",
     setup(){
-        const countryTag = ref('')
-        // const apiUri = `https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/${countryTag.value.toUpperCase()}.json`
-        const apiUri = `https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/PL.json`
-        const currentDate = new Date().getTime()
-        let data = ref('')
+        // packages
+        const moment = require('moment')
+
+        // vars
+        const currentDate = moment();
+        let countryTag = ref('');
+        let data = '';
+        let fData = ref([]);
+        
 
         const fetchData = async () => {
-            const promise = await fetch(apiUri)
-            const fdata = promise.json();
-
-            return fdata
+            const promise = await fetch(`https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/${countryTag.value.toUpperCase()}.json`)
+            const fetchedData = promise.json();
+            return fetchedData;
         }
 
         const displayData = () => {
-            console.log('fetching started')
-            fetchData().then((fdata) => {
-                data.value = fdata;
-                console.log('done', data.value)
+            fetchData().then((fetchedData) => {
+                data = fetchedData;
+                
+                fData.value = []
+                for(const comp of data.items){
+                    if(moment(currentDate).isSameOrBefore(comp.date.till)){
+                        fData.value.push(comp)
+                    }
+                }
             })
         }
 
         return{
             // vars
-            countryTag, data, currentDate,
+            countryTag, fData,
             // functions
-            displayData
+            displayData,
+            // packages
+            moment, countries
         }
     }
 }
 </script>
 
 <style>
+.compTxt{
+    color: white;
+    display: inline-block;
+}
 
+.compName{
+    font-weight: bold;
+    font-size: 24px;
+    margin-right: 5px;
+    text-decoration: underline;
+}
+
+.error{
+    color: red;
+}
 </style>
