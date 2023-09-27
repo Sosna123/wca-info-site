1e693900b13f7a8a4956053a4f25f813626cd3ad
Sosna123 <olek.arez@gmail.com>
2023-09-27 21:10:57 +0200

    /comps works!

--- src/views/Comps.vue
+++ src/views/Comps.vue
@@ -1,24 +1,29 @@
 <template>
-    <select name="countrySelect" v-model="countryTag">
-        <option disabled value="">Please select a country</option>
-        <option v-for="country in countries" :value="country.iso2Code">{{ country.name }}</option>
-    </select>
-    <button @click="displayData">button</button>
-    <ul>
-        <div>
-            <li v-for="comp in fData">
-                <a :href="`https://www.worldcubeassociation.org/competitions/${comp.id}`" target="_blank">
-                    <p class="compName compTxt">{{ comp.name }}</p>
-               </a>
-                <p class="compTxt">will be in {{ comp.city }} at {{ comp.venue.address }}</p>
-            </li>
-        </div>
-    </ul>
+    <div class="compsContainer">
+        <select name="countrySelect" v-model="countryTag">
+            <option disabled value="">Please select a country</option>
+            <option v-for="country in countries" :value="country.iso2Code">{{ country.name }}</option>
+        </select>
+        <button @click="displayData">Search for competitions</button>
+        <ul>
+            <div>
+                <div v-if="fData.length === 0">
+                    <p class="warringMessage">There is no competitions for now...</p>
+                </div>
+                <li v-else v-for="comp in fData">
+                    <a :href="`https://www.worldcubeassociation.org/competitions/${comp.id}`" target="_blank">
+                        <p class="compName compTxt">{{ comp.name }}</p>
+                    </a>
+                    <p class="compTxt">will be in {{ comp.city }} at {{ comp.venue.address }}</p>
+                </li>
+            </div>
+        </ul>
+    </div>
 </template>
 
 <script>
 import { ref } from 'vue';
-import { countries } from '../countries.js'
+import { countries } from '../js/countries.js'
 
 export default{
     name: "comps",
@@ -77,7 +82,32 @@ export default{
     text-decoration: underline;
 }
 
-.error{
-    color: red;
+.warringMessage{
+    color: orange;
+    font-size: 24px;
+}
+
+li{
+    color: white;
+}
+
+button{
+    background: #08f;
+    border: none;
+    padding: 10px 15px;
+    color: black;
+    border-radius: 15px;
+}
+
+select{
+    background: #fff;
+    border: none;
+    padding: 10px 20px;
+    color: black;
+    border-radius: 15px; 
+}
+
+.compsContainer{
+    margin: 0 0 0 20px;
 }
 </style>
